29119d0e5e21096357cb6706ddfd3e468ae45a18
github.repsu@passinbox.com <github.repsu@passinbox.com>
2024-01-27 20:15:57 +0000

    finalizacao da preparacao dos dados

--- ab_test.py
+++ ab_test.py
@@ -151,6 +151,87 @@ df1[['user_id', 'group', 'landing_page']].groupby( ['group', 'landing_page'] ).c
 
 # COMMAND ----------
 
+df1 = df1.drop_duplicates( subset='user_id' )
+
+df1[['user_id', 'group', 'landing_page']].groupby( ['group', 'landing_page'] ).count().reset_index()
+
+# COMMAND ----------
+
+df1['check'] = df1.apply( lambda x: 'right' if ( x['group'] == 'control' and x['landing_page'] == 'old_page' ) 
+                                    or ( x['group'] == 'treatment' and x['landing_page'] == 'new_page' ) 
+                                    else 'wrong', axis=1 )
+
+df1.head()
+
+# COMMAND ----------
+
+df_aux = df1.loc[df1['check'] == 'right']
+
+df_aux[['user_id', 'group', 'landing_page']].groupby( ['group', 'landing_page'] ).count().reset_index()
+
+# COMMAND ----------
+
 # MAGIC %md
-# MAGIC - Verificar o número único de user_ids e ( talvez ) remover duplicados
-# MAGIC - Pensar em uma maneira de remover os que viram uma página diferente da página esperada em razão do grupo que o user pertence
+# MAGIC ### Amostragem Aleatória dos Grupos
+
+# COMMAND ----------
+
+df2 = df_aux.drop( 'check', axis=1 )
+df2.head()
+
+# COMMAND ----------
+
+df_control = df2[df2['group'] == 'control' ].sample( n = sample_n, random_state=42 )
+print( f'Tamanho da amostra do Grupo de Controle: {df_control.shape[0]}' )
+
+df_treatment = df2[df2['group'] == 'treatment' ].sample( n = sample_n, random_state=42 )
+print( f'Tamanho da amostra do Grupo de Tratamento: {df_treatment.shape[0]}' )
+
+# COMMAND ----------
+
+df_ab = pd.concat( [df_control, df_treatment] ).reset_index( drop=True )
+df_ab.sample( 10 )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Conversão de cada Grupo
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Grupo de Controle
+
+# COMMAND ----------
+
+control_sales = df_control.loc[df_control['converted'] == 1, 'converted'].sum()
+control_visits = len( df_control )
+
+conversion_rate_control = np.divide( control_sales, control_visits )
+print( f'Conversão - Grupo de Controle: {conversion_rate_control}' )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Grupo de Tratamento
+
+# COMMAND ----------
+
+treatment_sales = df_treatment.loc[df_treatment['converted'] == 1, 'converted'].sum()
+treatment_visits = len( df_treatment )
+
+conversion_rate_treatment = np.divide( treatment_sales, treatment_visits )
+print( f'Conversão - Grupo de Tratamento: {conversion_rate_treatment}' )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Controle _vs._ Tratamento
+
+# COMMAND ----------
+
+conversion_rate_control
+conversion_rate_treatment
+
+df_vs = pd.DataFrame( {'Controle': conversion_rate_control, 'Tratamento': conversion_rate_treatment} , index=range( 1,2 ) )
+df_vs
